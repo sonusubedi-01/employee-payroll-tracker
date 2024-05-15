0610bdd8ea1eaf722302afbcd3f41652ea095424
Sonu Subedi <sonusubedi01@gmail.com>
2024-05-15 14:21:48 -0400

    select and display a random employee

--- script.js
+++ script.js
@@ -43,8 +43,11 @@ const displayAverageSalary = function(employeesArray) {
 }
 
 // Select a random employee
-const getRandomEmployee = function(employeesArray) {
-  // TODO: Select and display a random employee
+const getRandomEmployee = function (employeesArray) {
+  // Select and display a random employee
+  const randomIndex = Math.floor(Math.random()) * employeesArray.length;
+  const randomEmployee = employeesArray[randomIndex];
+  console.log(`Congratulations to ${randomEmployee.firstName} ${randomEmployee.lastName}, our random drawing winner!`)
 }
 
 /*
